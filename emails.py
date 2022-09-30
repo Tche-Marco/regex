37df0fbad7b5347a29394fd5b52107386a3ca193
Marco Antônio <marcomartins06@rede.ulbra.br>
2022-09-30 20:42:17 -0300

    Atualizações finais

--- emails.py
+++ emails.py
@@ -2,7 +2,7 @@ from noticias import get_ultimas_noticias
 
 
 def enviar_cotacao_e_noticias(valor_btc_atualizado) -> None:
-    """Por enquanto apenas imprime na tela o valor atualizado do BTC e as últimas notícias, mas o objetivo é mandar essas informações por email
+    """Por enquanto, apenas imprime na tela o valor atualizado do BTC e as últimas notícias, mas o objetivo é mandar essas informações por email
 
     Args:
         valor_btc_atualizado (float): Valor atualizado do BTC
